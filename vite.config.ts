ddf3fa564120008061feef1c34e7e3136abcaab5
xmdhs <xmdhss@gmail.com>
2022-08-12 01:07:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import legacy from '@vitejs/plugin-legacy'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), legacy({
-    targets: ['defaults', 'not IE 11']
+    targets: ['defaults', 'not IE 11'],
+    externalSystemJS: true
   }),
   ],
 })
